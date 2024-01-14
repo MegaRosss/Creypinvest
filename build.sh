e904926118fd1a27070aecf79e8085e186160292
MegaRosss <107857613+MegaRosss@users.noreply.github.com>
2024-01-14 04:44:18 +0100

    omom

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ pip install -r requirements_divine.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+
+echo "from django.contrib.auth.mode ls import User; User.objects.create_superuser('gandolfdgrey','barrymore@venax.co','Tomiwa@me123')" | python3 manage.py shell
